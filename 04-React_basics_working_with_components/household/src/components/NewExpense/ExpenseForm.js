4d42a8b4ad575d022882915e27e41f251cd2bd95
Tae Hyeon Jo <hillskuti@naver.com>
2022-05-25 21:20:35 +0900

    Listening to User Input

--- 04-React_basics_working_with_components/household/src/components/NewExpense/ExpenseForm.js
+++ 04-React_basics_working_with_components/household/src/components/NewExpense/ExpenseForm.js
@@ -2,13 +2,17 @@ import React from 'react';
 import './ExpenseForm.css';
 
 const ExpenseForm = () => {
+  const titleChangeHandler = (event) => {
+    console.log(event.target.value);
+  };
+
   return (
     <form action=''>
       <div className='new-expense__controls'>
         <div className='new-expense__control'>
           <label htmlFor=''>Title</label>
-          {/* 오프닝, 클로징 태그에 아무런 요소가 없기에 input은 셀프 클로징으로 가능하다. */}
-          <input type='text' />
+          {/* 오프닝, 클로징 태그에 아무런 요소가 없기에 input은 셀프 클로징으로 가능하다. onChange는 모든 키 입력에 반응하는 Event이다. */}
+          <input type='text' onChange={titleChangeHandler} />
         </div>
         <div className='new-expense__control'>
           <label htmlFor=''>Amount</label>
