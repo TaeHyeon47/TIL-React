754e79136b2cd4cb43c84674dddce7ddab748a0e
Tae Hyeon Jo <hillskuti@naver.com>
2022-05-26 08:57:12 +0900

    Using One State Instead (And What's Better)

--- 04-React_basics_working_with_components/household/src/components/NewExpense/ExpenseForm.js
+++ 04-React_basics_working_with_components/household/src/components/NewExpense/ExpenseForm.js
@@ -5,20 +5,48 @@ const ExpenseForm = () => {
   // useState에 초기값은 ''으로 String으로 저장한다.
   // onChage의 event.target.value 값이 항상 String으로 저장되기 때문이다.
   // Amount number가 들어가는 곳도 String으로 만들었다.
-  const [enteredTitle, setEnteredTitle] = useState('');
-  const [enteredAmount, setEnteredAmount] = useState('');
-  const [enteredDate, setEnteredDate] = useState('');
+  // const [enteredTitle, setEnteredTitle] = useState('');
+  // const [enteredAmount, setEnteredAmount] = useState('');
+  // const [enteredDate, setEnteredDate] = useState('');
+
+  // useState에 {}(object)를 사용하여, 그룹으로 만들어 줄 수 있다. 프로퍼티처럼 :을 사용하여 값을 입력한다.
+  // the logic is kind of the same but now it's in one state object managed as one piece of state
+  // instead of three separate slices. The difference is that whenever you update this state
+  // you need to update all three properties and not just one.
+  const [userInput, setUserInput] = useState({
+    enteredTitle: '',
+    enteredAmount: '',
+    enteredDate: '',
+  });
+
+  console.log({ ...userInput });
 
   const titleChangeHandler = (event) => {
-    setEnteredTitle(event.target.value);
+    // setEnteredTitle(event.target.value);
+
+    // State를 객체형태로 사용하기 위해서는 항상 ...으로 전체 객체를 풀어줘야한다.
+    setUserInput({
+      ...userInput, // takes an object pulls out all the key value pairs and adds them to this new object.
+      enteredTitle: event.target.value, // override "enteredTitle" key value pairs
+    });
   };
 
   const amountChangeHandler = (event) => {
-    setEnteredAmount(event.target.value);
+    // setEnteredAmount(event.target.value);
+
+    setUserInput({
+      ...userInput,
+      enteredAmount: event.target.value,
+    });
   };
 
   const DateChangeHandler = (event) => {
-    setEnteredDate(event.target.value);
+    // setEnteredDate(event.target.value);
+
+    setUserInput({
+      ...userInput,
+      enteredDate: event.target.value,
+    });
   };
 
   return (
@@ -34,7 +62,7 @@ const ExpenseForm = () => {
           {/* 'min' and 'step' are just default HTML attributes for input elements */}
           <input
             type='number'
-            onChange={titleChangeHandler}
+            onChange={amountChangeHandler}
             min='0.01'
             step='0.01'
           />
@@ -43,7 +71,7 @@ const ExpenseForm = () => {
           <label htmlFor=''>Date</label>
           <input
             type='date'
-            onChange={titleChangeHandler}
+            onChange={DateChangeHandler}
             min='2019-01-01'
             max='2022-12-31'
           />
