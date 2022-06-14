9fec3cf78fc488faec7927fc510f40d52015795e
Tae Hyeon Jo <hillskuti@naver.com>
2022-06-14 18:27:46 +0900

    Fetching Data

--- multipage-site/src/pages/Home.js
+++ multipage-site/src/pages/Home.js
@@ -1,15 +1,26 @@
 import React from 'react';
+import { useFetch } from '../hooks/useFetch';
+import './Home.css';
 
 export default function Home() {
+  const {
+    data: articles,
+    isPending,
+    error,
+  } = useFetch('http://localhost:3000/articles');
+
   return (
-    <div>
-      <h2>Homepage</h2>
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Animi dolorem,
-        repellendus qui consequuntur modi sit at dolores, iure eos id
-        dignissimos! Impedit temporibus laborum porro, rem quisquam doloribus
-        dolores illum?
-      </p>
+    <div className='home'>
+      <h2>Articles</h2>
+      {isPending && <div>Loading...</div>}
+      {error && <div>{error}</div>}
+      {articles &&
+        articles.map((article) => (
+          <div key={article.id} className='card'>
+            <h3>{article.title}</h3>
+            <p>{article.author}</p>
+          </div>
+        ))}
     </div>
   );
 }
